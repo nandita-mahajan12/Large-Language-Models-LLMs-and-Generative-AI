dcbbab5aa7b25c9a5d6811d2cbd4531092579c12
nandita-mahajan12 <nanditamahajan32@gmail.com>
2026-01-17 23:31:25 +0530

    Delete generate_text.py

--- generate_text.py
+++
@@ -1,9 +0,0 @@
-from transformers import pipeline
-
-generator = pipeline("text-generation", model="distilgpt2")
-
-promt = "AI will change the world because"
-
-result = generator(promt , max_length = 30, num_return_sequences=2)
-
-print(result)
